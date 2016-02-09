2b517ddd5513a8b3db56d551769c865cc1803664
p998dzz <domantas.zitkevicius@gmail.com>
2016-02-09 15:14:48 +0200

    Checked if calculator created and empty game return zero

--- test/com/swedbank/BowlingCalculatorTest.java
+++ test/com/swedbank/BowlingCalculatorTest.java
@@ -1,7 +1,29 @@
 package com.swedbank;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class BowlingCalculatorTest {
+    BowlingCalculator calc;
+
+    @Before
+    public void setup()
+    {
+        calc = new BowlingCalculator();
+    }
+
+    @Test
+    public void createCalculator() {
+        assertNotNull(calc);
+    }
+
+    @Test
+    public void emptyGameShouldReturnZeroResult() {
+        assertEquals("0", calc.getResult());
+    }
+
+    
 
 }
